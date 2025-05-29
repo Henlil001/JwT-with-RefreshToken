7a6b48707c5880ace0e03d89efb37f71bc39fc21
Henrik Liljestrand <henrik.liljestrand@hotmail.com>
2025-05-29 12:28:32 +0200

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwT_with_RefreshToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public 
+    }
+}
